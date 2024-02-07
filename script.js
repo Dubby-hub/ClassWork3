2fae998aabe109498a06577c35aac81cb8ecda16
sommypraise <sommypraise20@gmail.com>
2024-02-07 06:16:35 +0100

    new changes

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function togglePassword() {
     const passwordInput = document.getElementById("passwordInput");
     passwordInput.type === "password" ? passwordInput.type = "text" :passwordInput.type = "password";
 }
+
+
+function toglePassword() {
+    const passwordInput = document.getElementById("pswordInput");
+    passwordInput.type === "password" ? passwordInput.type = "text" :passwordInput.type = "password";
+}
